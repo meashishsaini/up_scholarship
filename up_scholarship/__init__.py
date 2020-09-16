c16bdbab2dcbfe6da0a324b93cc46c11c412ba65
Ashish Saini <sainiashish08@gmail.com>
2020-09-16 14:42:11 +0530

    Log to file
    Signed-off-by: Ashish Saini <sainiashish08@gmail.com>

---
+++ up_scholarship/__init__.py
@@ -0,0 +1,20 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+filename = os.path.dirname(__file__) + "/logs.log"
+
+# get handler
+rotating_file_handler = RotatingFileHandler(filename=filename)
+rotating_file_handler.setLevel(logging.INFO)
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# add formatter to handler
+rotating_file_handler.setFormatter(formatter)
+
+# get root logger
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+# add handler to logger
+root.addHandler(rotating_file_handler)
